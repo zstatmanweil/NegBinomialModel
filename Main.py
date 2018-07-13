ebbce64b303d633e23b6986d05e5734ced591240
Zoe Statman-Weil <zstatmanweil@gmail.com>
2018-07-12 18:04:58 -0700

    Continued to play around with residuals, including creating plots/figures. Also added an equation to chet pearson's chi2

--- Main.py
+++ Main.py
@@ -1,4 +1,3 @@
-import statsmodels.api as sm
 import pandas as pd
 from Variables import Variables
 from Model import Neg_Bin_Model
@@ -9,11 +8,11 @@ import matplotlib.pyplot as plt
 wd = os.chdir("C:\Users\zstat\Box Sync\WaterContamination\Data Analysis\Processed\Python")
 
 #Select the data set
-data = 'Dasymetric_Data.csv'
+#data = 'Dasymetric_Data.csv'
 #data = 'ArealWeighting_Data.csv'
-#data = 'Interpolation_data.csv'
+data = 'Interpolation_data.csv'
 
-#read relevant csv 
+#read relevant csv  
 df = pd.read_csv(data)
 
 #select violation data of interest
@@ -26,9 +25,9 @@ new_df.add_all_variables()
 complete_df = new_df.df
 
 #analyze data
-model = Neg_Bin_Model(complete_df, vio_data)
-
+model = Neg_Bin_Model(complete_df, vio_data, data)
 model.summarize()
+print "pearson chi2:", model.get_pearson()
 #model.get_mle_retvals()
 
 #get predictions
@@ -46,6 +45,20 @@ summary = Summary_Stats(complete_df, continuous_columns, cat_columns)
 #print summary.get_cont_summary_table(), "\n"
 #print summary.get_count_table()
 
-print model.df[["Predictions","Std_Pearson_Residuals","Std_Deviance_Residuals", "Residuals"]].head()
-plt.scatter(model.df["Predictions"], model.df["Std_Deviance_Residuals"])
-plt.show()
+#explore residuals
+pd.set_option('max_columns', 7)
+print model.df[["Predictions","Std_Pearson_Residuals","Std_Deviance_Residuals", "Residuals"]].head(5)
+
+#create plot of residuals
+# =============================================================================
+# plt.scatter(model.df["Predictions"], model.df["Std_Deviance_Residuals"], s=5, color="black")
+# plt.xlabel("Predicted Value of Mean Response")
+# plt.ylabel("Standardized Deviance Residuals")
+# plt.xlim([0,5])
+# plt.ylim([-3, 6])
+# =============================================================================
+
+#export DataFrame to csv
+model.df.to_csv("C:\Users\zstat\Box Sync\WaterContamination\Data Analysis"\
+                "\Processed\Python\NegBinomialModel\Export"\
+                "\\" + data[0:-9] + "_" + vio_data + ".csv")
--- Model.py
+++ Model.py
@@ -1,47 +1,86 @@
 import statsmodels.api as sm
+import statsmodels.formula.api as smf
 import numpy as np
+import pandas as pd
 
 class Neg_Bin_Model(object):
     
-    def __init__(self, df, violation_type):
+    def __init__(self, df, violation_type, data):
+        self.data = data
         self.df = df
         self.violation_type = violation_type
-            
-    def run(self):
+        
         #define the target and predictors 
-        target = self.df[self.violation_type]
-        predictors = self.df.loc[:,("Percent_Below_Poverty_Line", 
+        self.target = self.df[self.violation_type]
+        self.predictors = self.df.loc[:,("Percent_Below_Poverty_Line", 
                                       "Percent_Minority", 
                                       "Rural", 
                                       "Public",
                                       "ConnectionsLess200", 
                                       "GroundwaterOrCombined"
                                       )]
+            
+    def run(self):
+        X = sm.add_constant(self.predictors)
+        y = self.target
         
-        X = sm.add_constant(predictors)
-        y = target
-        #alpha=2.0174 (this is alpha in R)
-        model = sm.GLM(y,X,family=sm.families.NegativeBinomial())
-        #alternative: 
+# =============================================================================
+#         There are two ways to run the negative binomial regression using 
+#         statsmodel. One way provides a value for alpha (dispersion paramater), 
+#         and one does not but provides an easy way to get residuals. Thus, I 
+#         ran the way that provides the alpha for each dataset and recorded each below.
+# =============================================================================
+        alpha_data = {"All_Violations": [2.0174, 2.0165, 1.9930],
+                      "Violations_Yes_HealthBased": [3.8892, 3.8431, 3.8556]}
+        alpha_df = pd.DataFrame(alpha_data, index=['Dasymetric_Data.csv', 'ArealWeighting_Data.csv', 'Interpolation_data.csv'])
+        a = alpha_df.loc[self.data, self.violation_type]
+        
+        model = sm.GLM(y,X,family=sm.families.NegativeBinomial(alpha=a))
+        
+        #Method which provides alpha: 
         #model = sm.NegativeBinomial(y,X)
-        return model
-    
-    #def get_alpha():
+        return model      
     
     def summarize(self):        
-        print self.run().fit().summary2()
+        print self.run().fit(maxiter=100).summary2()
         
     def get_mle_retvals(self):
-        print self.run().fit().mle_retvals
+        print self.run().fit(maxiter=100).mle_retvals
+        
+    def get_pearson(self):
+        print self.run().fit(maxiter=100).pearson_chi2
         
     def get_predictions(self):
-        self.df["Predictions"] = self.run().predict(self.run().fit().params)
+        params = self.run().fit(maxiter=100).params
+        self.df["Predictions"] = self.run().predict(params)
                
     def get_residuals(self):
-        self.df["Residuals"] = self.run().fit().resid_response
+        self.df["Residuals"] = self.run().fit(maxiter=100).resid_response
         
     def get_pearson_residuals(self):
-        self.df["Std_Pearson_Residuals"] = self.run().fit().resid_pearson
+        self.df["Std_Pearson_Residuals"] = self.run().fit(maxiter=100).resid_pearson
         
     def get_deviance_residuals(self):
-        self.df["Std_Deviance_Residuals"] = self.run().fit().resid_deviance
+        self.df["Std_Deviance_Residuals"] = self.run().fit(maxiter=100).resid_deviance
+        
+# =============================================================================
+#     #to be deleted
+#     def ct_response(self, row):
+#         #Calculate response observation for Cameron-Trivedi dispersion test
+#         y = row[self.violation_type]
+#         m = row['vio_mu']
+#         return (((y - m)**2) - y) / m
+#     
+#     def get_alpha(self):
+#         X = sm.add_constant(self.predictors)
+#         y = self.target
+#         
+#         ct_data = self.df.copy()
+#         ct_data["vio_mu"] = sm.GLM(y,X,family=sm.families.Poisson()).fit().mu
+#         ct_data["ct_resp"] = ct_data.apply(self.ct_response, axis=1)
+#         # Linear regression of auxiliary formula
+#         ct_results = smf.ols('ct_resp ~ vio_mu - 1', ct_data).fit()
+#         # Construct confidence interval for alpha, the coefficient of vio_mu
+#         alpha_ci95 = ct_results.conf_int(0.05).loc['vio_mu']
+#         return ct_results.params[0] 
+# =============================================================================
